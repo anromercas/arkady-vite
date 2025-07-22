95448a83508912d7df584cf415bfde5be6212b45
Nuria Romero <anromercas@gmail.com>
2025-07-22 20:58:27 +0200

    remove vike

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
-import vike from 'vike/plugin';
 import path from 'path';
 
 export default defineConfig({
